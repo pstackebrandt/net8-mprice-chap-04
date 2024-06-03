3ef25e08730118a15b6e74f1ad65fa3377a4f5ad
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-06-03 20:30:11 +0200

    Instrumenting: Added: Write debug and trace messages additionally in file use Autoflush in Debug mode

--- Instrumenting/Program.cs
+++ Instrumenting/Program.cs
@@ -2,9 +2,30 @@
 
 using System.Diagnostics;
 
+var logPath = Path.Combine(
+    Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+
+WriteLine($"Writing to file at {logPath}");
+
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+
+Trace.Listeners.Add(logFile);
+
+#if DEBUG
+// Text writer is buffered, so this option calls
+// Flush() on all listeners after writing.
+
+Trace.AutoFlush = true;
+#endif
+
 Debug.WriteLine("Debug says, I am watching.");
 Trace.WriteLine("Trace says, I am watching.");
 // See Debug and Trace output in the Output window in Visual Studio
 // when running in Debug (!) mode
 Console.WriteLine("Console says, I send comments always.");
 
+// Debug and Trace messages are written to the file and the Output window!
+
+// Close the text file (also flushes) and releases resources.
+Debug.Close();
+Trace.Close();
