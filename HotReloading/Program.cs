8ba8afeae4b9ef2d00aa6d65ebc2252d6fe0ae08
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-07-30 22:35:42 +0200

    Adds project to train Hot Reloading

---
+++ HotReloading/Program.cs
@@ -0,0 +1,11 @@
+﻿
+/* Visual Studio 2022: run the app, change the message, click Hot Reload.
+   (It's not required to click Hot Reload if one saves the file, but it may be a good practice to do so.)
+   Visual Studio Code: run the app, using dotnet watch, change the message.
+*/
+
+while (true)
+{
+    WriteLine("Hello, Hot Reload! :-)");
+    await Task.Delay(2000);
+}
